14e5971d02027e7fbf0706ca2a7a7db5b636c7e4
campos4guzman <campos4guzman@users.noreply.github.com>
2019-11-25 13:48:32 +0100

    25/11

--- SpaceInvaders.js
+++ SpaceInvaders.js
@@ -1,9 +1,9 @@
 class Juego{
-
-
-
     creaCont(){
-        let cont=document.createElementNS();
+        let div=document.getElementById("contenedor");
+        let cont=document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+        cont.setAttribute("id","cont");
+        div.appendChild(cont);
     }
     mover(event){
 
@@ -18,5 +18,12 @@ class Juego{
 
 
 class Disparo{
-
+    constructor(){
+        this.x=;
+        this.y=;
+    }
 }
+
+
+var juego=new Juego();
+juego.creaCont();
